4b113321e8ee59ff7ad1992650b31ea9c71616db
Arnold Barna <arnold.barna@gmail.com>
2018-09-17 09:11:18 +0200

    chore(DataVendor): clean the mess up
    revert to .NET Framework (MS VisualBasic dll needs it)

---
+++ DataVendor/DataVendorOld/Repositories/IsinsCsvFileRepository.cs
@@ -0,0 +1,74 @@
+﻿using DataVendor.Models;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataVendor.Repositories
+{
+    public class IsinsCsvFileRepository
+    {
+        private const string separator = ",";
+        private const string _fileNameExtension = "csv";
+        private string _workingDirectory = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            "StockExchange");
+
+        private readonly Isins _isins = new Isins();
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IsinsCsvFileRepository()
+        {
+        }
+
+        /// <summary>
+        /// The directory in which the provider works.
+        /// </summary>
+        public IsinsCsvFileRepository(string workingDirectory) : this()
+        {
+            WorkingDirectory = workingDirectory;
+        }
+
+        /// <summary>
+        /// The directory in which the provider works.
+        /// </summary>
+        internal string WorkingDirectory
+        {
+            get => _workingDirectory;
+            set
+            {
+                if (!Directory.Exists(value))
+                {
+                    throw new Exception($"Invalid directory specified ({value})");
+                }
+                _workingDirectory = value;
+            }
+        }
+
+        /// <summary>
+        /// Loads the CSV file and stores its content.
+        /// </summary>
+        /// <returns></returns>
+        internal void Load()
+        {
+            var filePath = Path.Combine(_workingDirectory, "isins.csv");
+            using (var parser = new TextFieldParser(filePath, Encoding.UTF8))
+            {
+                parser.SetDelimiters(separator);
+
+                RemoveHeader(parser);
+
+                while (!parser.EndOfData)
+                {
+                    _isins.Add(parser.ReadFields());
+                }
+            }
+        }
+
+        private static void RemoveHeader(TextFieldParser parser) => parser.ReadLine();
+    }
+
+}
---
+++ DataVendor/DataVendorOld/Services/WebService.cs
@@ -0,0 +1,24 @@
+﻿using DataVendor.Models;
+using DataVendor.Repositories;
+
+namespace DataVendor.Services
+{
+    public class WebService
+    {
+        private readonly MarketDataCsvFileRepository _csvFileRepository;
+
+        public WebService()
+        {
+            _csvFileRepository = new MarketDataCsvFileRepository();
+        }
+
+        internal MarketDataEntities DownloadFromWeb()
+        {
+            return HtmlDownloader
+                .DownloadAll()
+                .GetMarketDataEntities();
+        }
+
+        internal void Update(MarketDataEntities latestData) => _csvFileRepository.Update(latestData);
+    }
+}
