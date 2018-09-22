386ea8f74697bdefac5e113920ccc2fa508925a8
Arnold Barna <arnold.barna@gmail.com>
2018-09-22 13:50:30 +0200

    test(CsvLineRegistryEntryWithIsin): add unit tests

---
+++ DataVendor/Repositories.UnitTests/CsvLineRegistryEntryWithIsin_FormatterForCsv.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Peter.Models.Enums;
+using Peter.Models.Implementations;
+using Peter.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Repositories.UnitTests
+{
+    [TestFixture]
+    public class CsvLineRegistryEntryWithIsin_FormatterForCsv
+    {
+        private readonly KeyValuePair<string, IRegistryEntry> _validregistryEntry = new KeyValuePair<string, IRegistryEntry>(
+            "DE0005408116",
+            new RegistryEntry
+            {
+                Name = "Aareal Bank AG",
+                OwnInvestorLink = new Uri("http://www.aareal-bank.com/investor-relations/"),
+                StockExchangeLink = new Uri("http://www.boerse-frankfurt.de/de/aktien/aareal+bank+ag+ag+DE0005408116"),
+                FinancialReport = new FinancialReport(2.08m, 6, DateTime.Now.AddDays(1).Date),
+                Position = Position.NoPosition
+            });
+        private readonly string _validResult = 
+            $@"Aareal Bank AG,DE0005408116,2.08,6,{DateTime.Now.AddDays(1).Date.ToString(new CultureInfo("us-EN"))},http://www.boerse-frankfurt.de/de/aktien/aareal+bank+ag+ag+DE0005408116,http://www.aareal-bank.com/investor-relations/,NoPosition";
+
+        [Test]
+        public void WithValidInput_ReturnsTrueAndValidResult() => 
+            Peter.Repositories.Helpers.CsvLineRegistryEntryWithIsin.FormatForCSV(_validregistryEntry, ",").Should().Be(_validResult);
+    }
+}
