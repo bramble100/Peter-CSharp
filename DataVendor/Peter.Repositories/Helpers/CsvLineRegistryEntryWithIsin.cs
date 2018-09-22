146a110035f71fa4ef3f5f56f6ac716be8ac8b6c
Arnold Barna <arnold.barna@gmail.com>
2018-09-22 10:15:18 +0200

    feat(Repositories): impement Load() method in RegistryCsvFileRepository class
    - add helper for it

---
+++ DataVendor/Peter.Repositories/Helpers/CsvLineRegistryEntryWithIsin.cs
@@ -0,0 +1,17 @@
+﻿using Peter.Models.Interfaces;
+using System.Collections.Generic;
+
+namespace Peter.Repositories.Helpers
+{
+    public class CsvLineRegistryEntryWithIsin
+    {
+        public static bool TryParseFromCsv(string[] input, out KeyValuePair<string, IRegistryEntry> result)
+        {
+            result = new KeyValuePair<string, IRegistryEntry>();
+
+            if (input.Length != 7) return false;
+
+            return true;
+        }
+    }
+}
--- DataVendor/Peter.Repositories/Implementations/RegistryCsvFileRepository.cs
+++ DataVendor/Peter.Repositories/Implementations/RegistryCsvFileRepository.cs
@@ -1,6 +1,11 @@
-﻿using Peter.Models.Interfaces;
+﻿using Microsoft.VisualBasic.FileIO;
+using Peter.Models.Implementations;
+using Peter.Models.Interfaces;
+using Peter.Repositories.Helpers;
 using Peter.Repositories.Interfaces;
 using System.Configuration;
+using System.IO;
+using System.Text;
 
 namespace Peter.Repositories.Implementations
 {
@@ -25,7 +30,23 @@ namespace Peter.Repositories.Implementations
 
         public IRegistry Load()
         {
-            throw new System.NotImplementedException();
+            var filePath = Path.Combine(_workingDirectory, _fileName);
+            IRegistry registry = new Registry();
+
+            using (var parser = new TextFieldParser(filePath, Encoding.UTF8))
+            {
+                parser.SetDelimiters(_separator);
+
+                RemoveHeader(parser);
+
+                while (!parser.EndOfData)
+                {
+                    if(CsvLineRegistryEntryWithIsin.TryParseFromCsv(parser.ReadFields(), out var result))
+                    registry.Add(result);
+                }
+            }
+
+            return registry;
         }
 
         public void Save(IRegistry entities)
