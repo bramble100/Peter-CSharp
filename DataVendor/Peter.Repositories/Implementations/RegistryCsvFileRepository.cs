e079f23959e50204174ded991f519a42d8533b80
Arnold Barna <arnold.barna@gmail.com>
2018-09-22 13:49:48 +0200

    feat(RegistryCsvFileRepository): implement Save() method

--- DataVendor/Peter.Repositories/Implementations/RegistryCsvFileRepository.cs
+++ DataVendor/Peter.Repositories/Implementations/RegistryCsvFileRepository.cs
@@ -3,8 +3,10 @@ using Peter.Models.Implementations;
 using Peter.Models.Interfaces;
 using Peter.Repositories.Helpers;
 using Peter.Repositories.Interfaces;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Peter.Repositories.Implementations
@@ -14,18 +16,6 @@ namespace Peter.Repositories.Implementations
         public RegistryCsvFileRepository() : base()
         {
             _fileName = new AppSettingsReader().GetValue("RegistryFileName", typeof(string)).ToString();
-
-            _header = new string[] 
-            {
-                "Name",
-                "ISIN",
-                "Stock Exchange Link",
-                "Own Investor Link",
-                "EPS",
-                "Months in Report",
-                "Next Report Date",
-                "Position"
-            };
         }
 
         public IRegistry Load()
@@ -51,7 +41,26 @@ namespace Peter.Repositories.Implementations
 
         public void Save(IRegistry entities)
         {
-            throw new System.NotImplementedException();
+            _header = new string[]
+            {
+                "Name",
+                "ISIN",
+                "Stock Exchange Link",
+                "Own Investor Link",
+                "EPS",
+                "Months in Report",
+                "Next Report Date",
+                "Position"
+            };
+
+            List<string> strings = AddHeader();
+
+            strings.AddRange(entities.Select(e => CsvLineRegistryEntryWithIsin.FormatForCSV(e, _separator)));
+
+            File.WriteAllLines(
+                Path.Combine(WorkingDirectory, _fileName),
+                strings,
+                Encoding.UTF8);
         }
     }
 }
