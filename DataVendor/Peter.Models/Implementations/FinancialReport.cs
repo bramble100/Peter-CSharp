2a69e1cefc9cb2aeb5bd53e949544547f857d18a
Arnold Barna <arnold.barna@gmail.com>
2018-09-22 10:56:01 +0200

    feat(Models.Implementations): add new classes
    - FinancialReport
    - RegistryEntry

---
+++ DataVendor/Peter.Models/Implementations/FinancialReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Peter.Models.Interfaces;
+
+namespace Peter.Models.Implementations
+{
+    public class FinancialReport : IFinancialReport
+    {
+        public FinancialReport()
+        {
+        }
+
+        public FinancialReport(decimal eps, int monthsInReport, DateTime nextReportDate)
+        {
+            if (new HashSet<int>() { 3, 6, 9, 12 }.Contains(monthsInReport) && nextReportDate > DateTime.Now)
+            {
+                EPS = eps;
+                MonthsInReport = monthsInReport;
+                NextReportDate = nextReportDate;
+            }
+        }
+
+        public decimal EPS { get; private set; }
+        public bool IsOutdated => NextReportDate > DateTime.Now;
+        public int MonthsInReport { get; private set; }
+        public DateTime NextReportDate { get; private set; } = DateTime.Now;
+    }
+}
---
+++ DataVendor/Peter.Models/Implementations/RegistryEntry.cs
@@ -0,0 +1,16 @@
+﻿using Peter.Models.Enums;
+using Peter.Models.Interfaces;
+using System;
+
+namespace Peter.Models.Implementations
+{
+    public class RegistryEntry : IRegistryEntry
+    {
+        public string Name { get; set; }
+        public Uri OwnInvestorLink { get; set; }
+        public Uri StockExchangeLink { get; set; }
+        public Position Position { get; set; }
+        public IFinancialReport FinancialReport { get; set; }
+        public IFinancialAnalysis FinancialAnalysis { get; set; }
+    }
+}
