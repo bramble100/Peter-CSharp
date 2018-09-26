d1f9f6cb3c1294576de2c3affa7407d2a6a2b171
Arnold Barna <arnold.barna@gmail.com>
2018-09-26 18:42:24 +0200

    feat(Repositories.Helpers): add CsvLineFinancialAnalysis class

---
+++ DataVendor/Peter.Repositories/Helpers/CsvLineFinancialAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using Peter.Models.Implementations;
+using Peter.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Peter.Repositories.Helpers
+{
+    public static class CsvLineFinancialAnalysis
+    {
+        public static string FormatForCSV(KeyValuePair<string, IFinancialAnalysis> e, string separator, CultureInfo cultureInfo) => 
+            string.Join(separator, new string[]
+            {
+                e.Value.Name.WrapWithQuotes(),
+                e.Key,
+            });
+    }
+}
