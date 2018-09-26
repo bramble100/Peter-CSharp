640e315ee2e0d8ee089d913f13f56dc2646f8204
Arnold Barna <arnold.barna@gmail.com>
2018-09-26 06:58:28 +0200

    feat(Models.Implementations): add FinancialAnalysis class

---
+++ DataVendor/Peter.Models/Implementations/FinancialAnalysis.cs
@@ -0,0 +1,14 @@
+﻿using Peter.Models.Interfaces;
+
+namespace Peter.Models.Implementations
+{
+    public class FinancialAnalysis : IFinancialAnalysis
+    {
+        public bool Buyable => throw new System.NotImplementedException();
+
+        public decimal ClosingPrice { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public decimal? FastSMA { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public decimal? SlowSMA { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public decimal PE { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
