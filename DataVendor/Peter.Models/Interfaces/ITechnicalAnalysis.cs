1f5ba653fec4af24abb96058b993364bd274ce0c
Arnold Barna <arnold.barna@gmail.com>
2018-09-30 16:32:43 +0200

    feat(Models.Interfaces): add and modify interfaces

---
+++ DataVendor/Peter.Models/Interfaces/ITechnicalAnalysis.cs
@@ -0,0 +1,17 @@
+﻿namespace Peter.Models.Interfaces
+{
+    /// <summary>
+    /// The indicators and numbers calculated from the market data.
+    /// </summary>
+    public interface ITechnicalAnalysis
+    {
+        /// <summary>
+        /// Fast Simple Moving Average.
+        /// </summary>
+        decimal FastSMA { get; set; }
+        /// <summary>
+        /// Slow Simple Moving Average.
+        /// </summary>
+        decimal SlowSMA { get; set; }
+    }
+}
