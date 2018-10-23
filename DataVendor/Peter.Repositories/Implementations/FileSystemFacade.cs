482ede0955107584324ae11473dfe2f064b1f071
Arnold Barna <arnold.barna@gmail.com>
2018-10-23 07:27:15 +0200

    refactor(Peter.Repositories) add file system abstraction
    - add IFileSystemFacade interface
    - add FileSystemFacade class

---
+++ DataVendor/Peter.Repositories/Implementations/FileSystemFacade.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using Peter.Repositories.Interfaces;
+
+namespace Peter.Repositories.Implementations
+{
+    internal class FileSystemFacade : IFileSystemFacade
+    {
+        public bool TryBackup(string fullPath, string backupFullPath, out string message)
+        {
+            message = string.Empty;
+
+            try
+            {
+                var backupDir = Path.GetDirectoryName(backupFullPath);
+                if (!Directory.Exists(backupDir))
+                {
+                    Directory.CreateDirectory(backupDir);
+                }
+
+                File.Move(fullPath, backupFullPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return false;
+            }
+        }
+
+        public bool TryLoad(string fullPath, out string content, out string message)
+        {
+            content = string.Empty;
+            message = string.Empty;
+
+            try
+            {
+                content = File.ReadAllText(fullPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return false;
+            }
+        }
+
+        public bool TrySave(string fullPath, string content, out string message)
+        {
+            message = string.Empty;
+
+            try
+            {
+                var dir = Path.GetDirectoryName(fullPath);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                File.WriteAllText(fullPath, content);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return false;
+            }
+        }
+    }
+}
