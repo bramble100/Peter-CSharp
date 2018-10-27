c1cf9802e7d76f9f3b5cd5158db2098a05979357
Arnold Barna <arnold.barna@gmail.com>
2018-10-27 10:25:39 +0200

    feat(Peter.Models): add generic IBuilder interface

---
+++ DataVendor/Peter.Models/Interfaces/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace Peter.Models.Interfaces
+{
+    interface IBuilder<T>
+    {
+        /// <summary>
+        /// Returns the constructed instance.
+        /// </summary>
+        /// <returns>The constructed instance</returns>
+        T Build();
+    }
+}
