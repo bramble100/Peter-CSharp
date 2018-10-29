b69f545226e3673ee8f2d247fdbff4581081da6b
Arnold Barna <arnold.barna@gmail.com>
2018-10-29 18:43:37 +0100

    feat(Models): add overrides to FinancialAnalysis and IFinancialAnalysis

--- DataVendor/Peter.Models/Implementations/FinancialAnalysis.cs
+++ DataVendor/Peter.Models/Implementations/FinancialAnalysis.cs
@@ -16,5 +16,20 @@ namespace Peter.Models.Implementations
         }
 
         public decimal PE { get; set; }
+
+        public override bool Equals(object obj) => Equals(obj as FinancialAnalysis);
+
+        public bool Equals(IFinancialAnalysis other)
+        {
+            return other != null &&
+                   PE == other.PE;
+        }
+
+        public override int GetHashCode()
+        {
+            return 380272302 + PE.GetHashCode();
+        }
+
+        public override string ToString() => $"{PE}";
     }
 }
