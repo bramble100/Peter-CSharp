7c1c9354c643ace70f32e3a18f100f447bd270d7
Arnold Barna <arnold.barna@gmail.com>
2018-10-29 18:40:12 +0100

    feat(Models): add overrides to Analysis and IAnalysis

--- DataVendor/Peter.Models/Implementations/Analysis.cs
+++ DataVendor/Peter.Models/Implementations/Analysis.cs
@@ -1,4 +1,6 @@
 ﻿using Peter.Models.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace Peter.Models.Implementations
 {
@@ -9,5 +11,30 @@ namespace Peter.Models.Implementations
         public int QtyInBuyingPacket { get; set; }
         public ITechnicalAnalysis TechnicalAnalysis { get; set; }
         public IFinancialAnalysis FinancialAnalysis { get; set; }
+
+        public override bool Equals(object obj) => Equals(obj as Analysis);
+
+        public bool Equals(IAnalysis other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   ClosingPrice == other.ClosingPrice &&
+                   QtyInBuyingPacket == other.QtyInBuyingPacket &&
+                   EqualityComparer<ITechnicalAnalysis>.Default.Equals(TechnicalAnalysis, other.TechnicalAnalysis) &&
+                   EqualityComparer<IFinancialAnalysis>.Default.Equals(FinancialAnalysis, other.FinancialAnalysis);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1319828343;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + ClosingPrice.GetHashCode();
+            hashCode = hashCode * -1521134295 + QtyInBuyingPacket.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<ITechnicalAnalysis>.Default.GetHashCode(TechnicalAnalysis);
+            hashCode = hashCode * -1521134295 + EqualityComparer<IFinancialAnalysis>.Default.GetHashCode(FinancialAnalysis);
+            return hashCode;
+        }
+
+        public override string ToString() => $"{Name} {ClosingPrice} {QtyInBuyingPacket}";
     }
 }
