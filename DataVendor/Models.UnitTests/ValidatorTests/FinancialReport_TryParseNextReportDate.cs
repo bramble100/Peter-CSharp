0ccd67ded0ebd3e70c1ebe1c19301d9a3d76f435
Arnold Barna <arnold.barna@gmail.com>
2019-01-16 20:55:00 +0100

    test(Models.UnitTests): add validator tests

---
+++ DataVendor/Models.UnitTests/ValidatorTests/FinancialReport_TryParseNextReportDate.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Peter.Models.Validators;
+using System;
+
+namespace Models.UnitTests.ValidatorTests
+{
+    [TestFixture]
+    public class FinancialReport_TryParseNextReportDate
+    {
+        private DateTime _expectedResult = new DateTime(3000, 1, 1);
+
+        [TestCase("3000.01.01")]
+        public void ReturnsTrue_WhenInputIsValid(string input)
+        {
+            FinancialReport.TryParseNextReportDate(input, out var result).Should().BeTrue();
+            result.Should().Be(_expectedResult);
+        }
+
+        [TestCase("")]
+        [TestCase("x")]
+        public void ReturnsFalse_WhenInputIsInvalid(string input) =>
+            FinancialReport.TryParseNextReportDate(input, out var result).Should().BeFalse();
+    }
+}
