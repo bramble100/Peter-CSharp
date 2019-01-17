2add32e5748f95ace39e9f8b16fad04e92bba9c4
Arnold Barna <arnold.barna@gmail.com>
2019-01-17 19:58:53 +0100

    test(Models.Validators): add MarketDataEntity.TryParseClosingPrice tests

---
+++ DataVendor/Models.UnitTests/ValidatorTests/MarketDataEntity_TryParseClosingPrice.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Peter.Models.Validators;
+using System.Globalization;
+
+namespace Models.UnitTests.ValidatorTests
+{
+    [TestFixture]
+    public class MarketDataEntity_TryParseClosingPrice
+    {
+        [TestCase("1,8", "hu-HU", 1.8)]
+        [TestCase("1.8", "en-US", 1.8)]
+        public void ReturnsTrue_WhenInputIsValid(
+            string input, 
+            string cultureInfoString, 
+            decimal expected)
+        {
+            MarketDataEntity
+                .TryParseClosingPrice(
+                    input, 
+                    new CultureInfo(cultureInfoString), 
+                    out var result)
+                .Should()
+                .BeTrue();
+            result.Should().Be(expected);
+        }
+
+        [TestCase("", "hu-HU", 1.8)]
+        [TestCase("", "en-US", 1.8)]
+        [TestCase("x", "hu-HU", 1.8)]
+        [TestCase("x", "en-US", 1.8)]
+        [TestCase("1.8", "hu-HU", 1.8)]
+        public void ReturnsFalse_WhenInputIsInvalid(
+            string input, 
+            string cultureInfoString, 
+            decimal expected)
+        {
+            MarketDataEntity
+                .TryParseClosingPrice(
+                    input, 
+                    new CultureInfo(cultureInfoString), 
+                    out var result)
+                .Should()
+                .BeFalse();
+        }
+    }
+}
