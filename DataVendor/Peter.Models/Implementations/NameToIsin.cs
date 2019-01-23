6fbc788789dc385a9cba519c1e50abba5050378d
Arnold Barna <arnold.barna@gmail.com>
2019-01-23 20:13:35 +0100

    feat(Models): add INameToIsin and implementation

---
+++ DataVendor/Peter.Models/Implementations/NameToIsin.cs
@@ -0,0 +1,10 @@
+﻿using Peter.Models.Interfaces;
+
+namespace Peter.Models.Implementations
+{
+    public class NameToIsin : INameToIsin
+    {
+        public string Name { get; set; }
+        public string Isin { get; set; }
+    }
+}
---
+++ DataVendor/Peter.Models/Interfaces/INameToIsin.cs
@@ -0,0 +1,17 @@
+﻿namespace Peter.Models.Interfaces
+{
+    /// <summary>
+    /// A key-value pair to contain the ISIN by the company name. Key: Company name; Value: ISIN
+    /// </summary>
+    public interface INameToIsin
+    {
+        /// <summary>
+        /// Company name.
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// ISIN.
+        /// </summary>
+        string Isin { get; set; }
+    }
+}
