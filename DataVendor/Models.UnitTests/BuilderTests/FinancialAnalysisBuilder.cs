ce4c577d6a7bcd247acda6e5ca7947992395d6e9
Arnold Barna <arnold.barna@gmail.com>
2019-01-23 23:07:53 +0100

    test(Models.UnitTests.BuilderTests): add FinancialAnalysisBuilder tests

---
+++ DataVendor/Models.UnitTests/BuilderTests/FinancialAnalysisBuilder.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Models.UnitTests.BuilderTests
+{
+    [TestFixture]
+    public class FinancialAnalysisBuilder
+    {
+        [TestCase(24, 4, 3, 1.5)]
+        public void ShouldReturnAnalysis_WhenInputValid(
+            decimal price, 
+            decimal eps, 
+            int months, 
+            decimal expectedPE)
+        {
+            var result = new Peter.Models.Builders.FinancialAnalysisBuilder()
+                .SetClosingPrice(price)
+                .SetEPS(eps)
+                .SetMonthsInReport(months)
+                .Build();
+            result.PE.Should().Be(expectedPE);
+        }
+
+        [TestCase(0, 4, 3)] // invalid price
+        [TestCase(-1, 4, 3)] // invalid price
+        [TestCase(24, 0, 3)] // zero eps
+        [TestCase(24, 4, 0)] // invalid months
+        [TestCase(24, 4, 2)] // invalid months
+        public void ShouldReturnNull_WhenInputInvalid(
+            decimal price,
+            decimal eps,
+            int months)
+        {
+            var result = new Peter.Models.Builders.FinancialAnalysisBuilder()
+                .SetClosingPrice(price)
+                .SetEPS(eps)
+                .SetMonthsInReport(months)
+                .Build();
+
+            result.Should().BeNull();
+        }
+    }
+}
