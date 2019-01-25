4bee3979e5eb9de865c8802e7d9839c5dfba212a
Arnold Barna <arnold.barna@gmail.com>
2019-01-25 18:35:58 +0100

    feat(Models.Builders): improve FinancialReportBuilder

--- DataVendor/Peter.Models/Builders/FinancialReportBuilder.cs
+++ DataVendor/Peter.Models/Builders/FinancialReportBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Peter.Models.Implementations;
+﻿using NLog;
+using Peter.Models.Implementations;
 using Peter.Models.Interfaces;
 using System;
 using System.Globalization;
@@ -7,6 +8,8 @@ namespace Peter.Models.Builders
 {
     public class FinancialReportBuilder : IBuilder<IFinancialReport>
     {
+        private readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+
         private readonly CultureInfo _cultureInfo;
 
         private bool _EPSset;
@@ -29,48 +32,58 @@ namespace Peter.Models.Builders
 
         public FinancialReportBuilder SetEPS(string value)
         {
+            if (string.IsNullOrWhiteSpace(value)) return this;
+
             try
             {
                 _eps = Convert.ToDecimal(value, _cultureInfo);
                 _EPSset = true;
             }
-            catch (FormatException)
+            catch (FormatException ex)
             {
+                _logger.Warn(ex);
             }
+
             return this;
         }
 
         public FinancialReportBuilder SetMonthsInReport(string value)
         {
+            if (string.IsNullOrWhiteSpace(value)) return this;
+
             try
             {
                 _monthsInReport = Convert.ToInt32(value);
                 _monthsInReportSet = true;
             }
-            catch (FormatException)
+            catch (FormatException ex)
             {
+                _logger.Warn(ex);
             }
+
             return this;
         }
 
         public FinancialReportBuilder SetNextReportDate(string value)
         {
+            if (string.IsNullOrWhiteSpace(value)) return this;
+
             try
             {
                 _nextReportDate = Convert.ToDateTime(value, _cultureInfo);
                 _nextReportDateSet = true;
             }
-            catch (FormatException)
+            catch (FormatException ex)
             {
+                _logger.Warn(ex);
             }
+
             return this;
         }
 
-        public IFinancialReport Build()
-        {
-            return _EPSset && _monthsInReportSet && _nextReportDateSet 
-                ? new FinancialReport(_eps, _monthsInReport, _nextReportDate) 
+        public IFinancialReport Build() => 
+            _EPSset && _monthsInReportSet && _nextReportDateSet
+                ? new FinancialReport(_eps, _monthsInReport, _nextReportDate)
                 : null;
-        }
     }
 }
