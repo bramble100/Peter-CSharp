f869f0f87f0d41afcfccba3565f0c509feb3bb5e
Arnold Barna <arnold.barna@gmail.com>
2019-02-02 09:03:55 +0100

    test(Repositories.IntegrationTests): add new test

---
+++ DataVendor/Repositories.IntegrationTests/IsinsCsvFileRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Peter.Repositories.Implementations;
+using Peter.Repositories.Interfaces;
+
+namespace Repositories.IntegrationTests
+{
+    [TestFixture]
+    public class IsinsCsvFileRepositoryTests
+    {
+        [Test]
+        public void ContainsName()
+        {
+            var mock = new Mock<IFileSystemFacade>();
+            mock
+                .Setup(facade => facade.Load("mock"))
+                .Returns("Name;ISIN\n\"1+1 DRILLISCH AG O.N.\"; DE0005545503");
+            IIsinsRepository repository = new IsinsCsvFileRepository(mock.Object);
+            var result = repository.GetNames();
+            result.Should().Contain("1+1 DRILLISCH AG O.N.");
+            result.Should().HaveCount(1);
+        }
+    }
+}
