bdc36eea07a7323d5e0ccb11193d5543a88eb596
Arnold Barna <arnold.barna@gmail.com>
2019-02-04 20:34:37 +0100

    feat(Peter): add new project Infrastructure and Config

---
+++ DataVendor/Infrastructure/Config.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure
+{
+    public class Config : IConfig
+    {
+        public T GetValue<T>(string key)
+        {
+            Dictionary<string, object> _configuration = new Dictionary<string, object>()
+            {
+                { "BackupDirectory", "Backups" },
+                { "CsvFileNameExtension", "csv"},
+                { "CsvSeparator", ";"},
+                { "CultureInfo", "hu-HU"},
+                { "DateFormatForFileName", "yyyy-MM-dd-HH-mm"},
+                { "FetchNewMarketData", "fetch" },
+                { "IsinFileName", "isins.csv" },
+                { "MarketDataFileName", "marketdata.csv" },
+                { "RegistryFileName", "registry.csv" },
+                { "UpdateMarketDataWithISINs", "addisins" },
+                { "WorkingDirectory", "StockExchange" },
+                { "WorkingDirectoryBase", "Desktop" },
+                { "WorkingDirectoryRawDownloads", "RawDownload" }
+            };
+
+            return (T)_configuration[key];
+        }
+    }
+}
