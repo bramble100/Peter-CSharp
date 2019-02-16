74403e4a92b171a4f008361c3d70b8080cffe55a
Arnold Barna <arnold.barna@gmail.com>
2019-02-16 10:07:18 +0100

    test(AnalysesManager.UnitTests): add ReturnsCorrectResult()

--- DataVendor/AnalysesManager.UnitTests/ServiceTests_GetTrend.cs
+++ DataVendor/AnalysesManager.UnitTests/ServiceTests_GetTrend.cs
@@ -1,6 +1,7 @@
 ﻿using AnalysesManager.Services.Implementations;
 using NUnit.Framework;
 using Peter.Models.Builders;
+using Peter.Models.Enums;
 using System;
 
 namespace AnalysesManager.UnitTests
@@ -8,7 +9,7 @@ namespace AnalysesManager.UnitTests
     public class ServiceTests_GetTrend
     {
         [Test]
-        public void GetTrend_ThrowsArgumentNullException()
+        public void ThrowsArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => Service.GetTrend(null));
         }
@@ -17,7 +18,7 @@ namespace AnalysesManager.UnitTests
         [TestCase(-1, 1)]
         [TestCase(1, 0)]
         [TestCase(1, -1)]
-        public void GetTaz_ThrowsArgumentException(decimal fastSMA, decimal slowSMA)
+        public void ThrowsArgumentException(decimal fastSMA, decimal slowSMA)
         {
             var analysis = new TechnicalAnalysisBuilder()
                     .SetFastSMA(1)
@@ -28,6 +29,22 @@ namespace AnalysesManager.UnitTests
             analysis.SlowSMA = slowSMA;
 
             Assert.Throws<ArgumentException>(() => Service.GetTrend(analysis));
+        }
+
+        [TestCase(3.1, 2.1, Trend.Up)]
+        [TestCase(2.1, 3.1, Trend.Down)]
+        [TestCase(2.1, 2.1, Trend.Undefined)]
+        public void ReturnsCorrectResult(
+            decimal fastSMA,
+            decimal slowSMA,
+            Trend expectedResult)
+        {
+            var analysis = new TechnicalAnalysisBuilder()
+                    .SetFastSMA(fastSMA)
+                    .SetSlowSMA(slowSMA)
+                    .Build();
+
+            Assert.AreEqual(expectedResult, Service.GetTrend(analysis));
         }
     }
 }
