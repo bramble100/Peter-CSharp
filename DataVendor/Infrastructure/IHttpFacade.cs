daacd61dde165976fe26b84171ced9d422675e3a
Arnold Barna <arnold.barna@gmail.com>
2019-03-02 09:38:29 +0100

    feat(Infrastructure): add HTTP
    - IHttpFacade interface
    - HttpFacade implementation

---
+++ DataVendor/Infrastructure/IHttpFacade.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// Provides access to the Web via HTTP.
+    /// </summary>
+    public interface IHttpFacade
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpClient"/> class.
+        /// </summary>
+        /// <returns></returns>
+        HttpClient GetHttpClient();
+    }
+}
